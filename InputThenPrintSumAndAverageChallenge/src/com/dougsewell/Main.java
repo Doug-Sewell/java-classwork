dff1323079dc4f9b4f2f03b4b4b370752a875913
Doug Sewell <DouglasASewell@gmail.com>
2020-02-09 12:24:47 -0500

    Fixed issues. Output is now correct to challenge directions.

--- InputThenPrintSumAndAverageChallenge/src/com/dougsewell/Main.java
+++ InputThenPrintSumAndAverageChallenge/src/com/dougsewell/Main.java
@@ -6,29 +6,27 @@ public class Main {
 
     public static void main(String[] args) {
         inputThenPrintSumAndAverage();
+        inputThenPrintSumAndAverage();
     }
 
     public static void inputThenPrintSumAndAverage() {
         int counter = 0;
         int sum = 0;
+
         Scanner scanner = new Scanner(System.in);
         boolean validInput = true;
-        boolean isAnInt;
 
         while(validInput) {
-            System.out.println("Please enter a number to add.");
-            isAnInt = scanner.hasNextInt();
-            if(isAnInt) {
+            if(scanner.hasNextInt()) {
                 counter++;
                 sum += scanner.nextInt();
             } else if(counter == 0) {
-                System.out.println("SUM=0 AVG=0");
+                System.out.println("SUM = 0 AVG = 0");
                 validInput = false;
             }else {
-                System.out.println("SUM = " + sum + " AVG = " + (sum/counter));
+                System.out.println("SUM = " + sum + " AVG = " + (Math.round(((double)sum/(double)counter))));
                 validInput = false;
             }
         }
-        scanner.close();
     }
 }
