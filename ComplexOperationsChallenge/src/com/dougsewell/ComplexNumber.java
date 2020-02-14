c51ac0f58b14707bee090ad0da9fec07487d6b38
Doug Sewell <DouglasASewell@gmail.com>
2020-02-13 22:33:01 -0500

    Completed A new course challenge.

---
+++ ComplexOperationsChallenge/src/com/dougsewell/ComplexNumber.java
@@ -0,0 +1,39 @@
+package com.dougsewell;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber other) {
+        this.real += other.getReal();
+        this.imaginary += other.getImaginary();
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber other) {
+        real -= other.getReal();
+        imaginary -= other.getImaginary();
+    }
+}
